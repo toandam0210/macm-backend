a705e880946a7d542eae9e08b72f84b2cd0cec04
DESKTOP-RJUK2FF\VAN TOAN <toandam0210@gmail.com>
2022-05-30 11:00:57 +0700

    first commit

---
+++ src/main/java/com/fpt/macm/MacmApplication.java
@@ -0,0 +1,13 @@
+package com.fpt.macm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MacmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MacmApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fpt/macm/MacmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fpt.macm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MacmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
