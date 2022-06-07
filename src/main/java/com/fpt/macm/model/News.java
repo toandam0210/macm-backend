64fc3d400199054f6ddeeb02398f904ca0809f14
DESKTOP-RJUK2FF\VAN TOAN <toandam0210@gmail.com>
2022-06-08 01:40:11 +0700

    test heroku entity

--- src/main/java/com/fpt/macm/model/News.java
+++ src/main/java/com/fpt/macm/model/News.java
@@ -10,7 +10,7 @@ import javax.persistence.Id;
 import javax.persistence.Table;
 
 @Entity
-@Table(name = "news")
+@Table(name = "`news`")
 public class News {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
