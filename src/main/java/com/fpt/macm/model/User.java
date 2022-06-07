492b531e1b4ce2c592e013c600521bdcfc58db33
DESKTOP-RJUK2FF\VAN TOAN <toandam0210@gmail.com>
2022-06-08 02:14:00 +0700

    change name of table

--- src/main/java/com/fpt/macm/model/User.java
+++ src/main/java/com/fpt/macm/model/User.java
@@ -18,7 +18,7 @@ import javax.persistence.OneToMany;
 import javax.persistence.Table;
 
 @Entity
-@Table(name = "`user`")
+@Table(name = "users")
 public class User {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
