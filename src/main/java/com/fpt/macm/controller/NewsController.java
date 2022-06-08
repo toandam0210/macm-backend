e22a3e0cac4ffaddd8ae298d39d92cd16ef5daa4
Pham Minh Duc <menly10x@github.com>
2022-06-08 22:52:37 +0700

    delete news, sửa api cho news controller

--- src/main/java/com/fpt/macm/controller/NewsController.java
+++ src/main/java/com/fpt/macm/controller/NewsController.java
@@ -3,6 +3,7 @@ package com.fpt.macm.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -22,19 +23,24 @@ public class NewsController {
 	@Autowired
 	NewsService newsService;
 	
-	@PostMapping("/communication/createnews")
+	@PostMapping("/headcommunication/createnews")
 	ResponseEntity<ResponseMessage> createNews(@RequestBody News news, @RequestParam boolean isSendNotification){
 		return new ResponseEntity<ResponseMessage>(newsService.createNews(news, isSendNotification), HttpStatus.OK);
 	}
 	
-	@GetMapping("/communication/getallnews")
+	@GetMapping("/headcommunication/getallnews")
 	ResponseEntity<ResponseMessage> getAllNews(@RequestParam(defaultValue = "0") int pageNo,
 			@RequestParam(defaultValue = "10") int pageSize, @RequestParam(defaultValue = "id") String sortBy){
 		return new ResponseEntity<ResponseMessage>(newsService.getAllNews(pageNo, pageSize, sortBy), HttpStatus.OK);
 	}
 	
-	@GetMapping("/communication/getnewsbyid/{newsId}")
+	@GetMapping("/headcommunication/getnewsbyid/{newsId}")
 	ResponseEntity<ResponseMessage> getNewsById(@PathVariable(name = "newsId") int newsId){
 		return new ResponseEntity<ResponseMessage>(newsService.getNewsById(newsId), HttpStatus.OK);
+	}
+	
+	@DeleteMapping("/headcommunication/deletenewsbyid/{newsId}")
+	ResponseEntity<ResponseMessage> deleteNewsById(@PathVariable(name = "newsId") int newsId){
+		return new ResponseEntity<ResponseMessage>(newsService.deleteNewsById(newsId), HttpStatus.OK);
 	}
 }
