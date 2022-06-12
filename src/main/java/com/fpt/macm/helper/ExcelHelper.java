76902c11c8109eab860459f62da9b2b4130e9bb5
DESKTOP-RJUK2FF\VAN TOAN <toandam0210@gmail.com>
2022-06-12 23:09:58 +0700

    import excelfile

---
+++ src/main/java/com/fpt/macm/helper/ExcelHelper.java
@@ -0,0 +1,7 @@
+package com.fpt.macm.helper;
+
+public class ExcelHelper {
+	public static String TYPE = "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet";
+	static String[] HEADERs = { "MSSV", "Tên", "Ngày sinh", "SĐT", "Email", "Giới tính","Trạng thái hoạt động","Vai trò","Địa chỉ"};
+	static String SHEET = "Users";
+}
