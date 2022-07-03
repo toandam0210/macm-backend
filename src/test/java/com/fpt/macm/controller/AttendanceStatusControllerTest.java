99f3fd7af80e9913ea62d4f0f658574b1e523e66
DESKTOP-RJUK2FF\VAN TOAN <toandam0210@gmail.com>
2022-07-04 02:31:51 +0700

    attendance status controller unit test

---
+++ src/test/java/com/fpt/macm/controller/AttendanceStatusControllerTest.java
@@ -0,0 +1,68 @@
+package com.fpt.macm.controller;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.multipart;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+
+import com.fpt.macm.model.Constant;
+
+
+@SpringBootTest
+public class AttendanceStatusControllerTest {
+	@Autowired
+	private WebApplicationContext context;
+
+	private MockMvc mockMvc;
+
+	@InjectMocks
+	AttendanceStatusController attendanceStatusController;
+
+	@BeforeEach
+	public void setup() throws Exception {
+		this.mockMvc = MockMvcBuilders.webAppContextSetup(this.context).build();
+	}
+	
+	@Test
+	public void takeAttendanceSuccessTest() throws Exception {
+		this.mockMvc.perform(put("/api/admin/headtechnique/takeattendance/{studentId}", "HE140855")
+		.contentType(MediaType.APPLICATION_JSON))
+		.andExpect(status().isOk())
+		.andExpect(content()
+				.contentType(MediaType.APPLICATION_JSON))
+		.andExpect(jsonPath("$.message").value(Constant.MSG_056));
+	}
+	
+	@Test
+	public void checkAttendanceSuccessTest() throws Exception {
+		this.mockMvc.perform(get("/api/admin/headtechnique/checkattendance/{trainingScheduleId}", 1)
+		.contentType(MediaType.APPLICATION_JSON))
+		.andExpect(status().isOk())
+		.andExpect(content()
+				.contentType(MediaType.APPLICATION_JSON))
+		.andExpect(jsonPath("$.data.size()").value("37"));
+	}
+	
+	@Test
+	public void attendanceReportSuccessTest() throws Exception {
+		this.mockMvc.perform(get("/api/admin/headtechnique/checkattendance/report").param("semester", "Summer2022")
+		.contentType(MediaType.APPLICATION_JSON))
+		.andExpect(status().isOk())
+		.andExpect(content()
+				.contentType(MediaType.APPLICATION_JSON))
+		.andExpect(jsonPath("$.data.size()").value("19"));
+	}
+}
--- src/test/java/com/fpt/macm/controller/UserControllerTest.java
+++ src/test/java/com/fpt/macm/controller/UserControllerTest.java
@@ -10,7 +10,8 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 import java.nio.file.Files;
 import java.nio.file.Paths;
-import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -24,9 +25,8 @@ import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fpt.macm.dto.InforInQrCode;
 import com.fpt.macm.dto.UserDto;
-import com.fpt.macm.model.Role;
-import com.fpt.macm.model.User;
 import com.fpt.macm.repository.UserRepository;
 import com.fpt.macm.service.UserService;
 
@@ -87,6 +87,56 @@ public class UserControllerTest {
 		user.setRoleId(1);
 		user.setGeneration(4);
 		return user;
+	}
+	
+	private List<UserDto> usersDto(){
+		List<UserDto> users = new ArrayList<UserDto>();
+		UserDto user = new UserDto();
+		user.setStudentId("HE140855");
+		user.setName("Dam Van Toan");
+		user.setGender(true);
+//		LocalDate localDate = LocalDate.of(2000, 02, 10);
+//		user.setDateOfBirth(localDate);
+		user.setEmail("toandvhe140855@fpt.edu.vn");
+		user.setImage(null);
+		user.setPhone("0982102000");
+		user.setActive(true);
+		user.setCurrentAddress("Dom A");
+		user.setRoleId(1);
+		user.setGeneration(4);
+		
+		UserDto user2 = new UserDto();
+		user2.setStudentId("HE140856");
+		user2.setName("dam van toan 02");
+		user2.setGender(true);
+//		LocalDate localDate = LocalDate.of(2000, 02, 10);
+//		user.setDateOfBirth(localDate);
+		user2.setEmail("toandvhe140856@fpt.edu.vn");
+		user2.setImage(null);
+		user2.setPhone("0982102001");
+		user2.setActive(true);
+		user2.setCurrentAddress("Dom A");
+		user2.setRoleId(2);
+		user2.setGeneration(4);
+		
+		UserDto user3 = new UserDto();
+		user3.setStudentId("HA140857");
+		user3.setName("dam van tuan 02");
+		user3.setGender(false);
+//		LocalDate localDate = LocalDate.of(2000, 02, 10);
+//		user.setDateOfBirth(localDate);
+		user3.setEmail("tuandvhe140856@fpt.edu.vn");
+		user3.setImage(null);
+		user3.setPhone("0982102002");
+		user3.setActive(false);
+		user3.setCurrentAddress("Dom A");
+		user3.setRoleId(2);
+		user3.setGeneration(3);
+		
+		users.add(user2);
+		users.add(user);
+		users.add(user3);
+		return users;
 	}
 	@Test
 	public void getUserByStudentIdTestSuccess() throws Exception {
@@ -153,7 +203,7 @@ public class UserControllerTest {
 	public void updateUserTestFailEmail() throws Exception {
 		UserDto userDto = createUserDto();
 		userDto.setEmail("tuanlahe141277@fpt.edu.vn");
-		this.mockMvc.perform(put("/api/admin/hr/updateuser/{studentId}", "HE140855")
+		this.mockMvc.perform(put("/api/admin/hr/updateuser/{studentId}", "HE1408558")
 		.content(asJsonString(userDto))
 		.contentType(MediaType.APPLICATION_JSON))
 		.andExpect(status().isOk())
@@ -167,7 +217,7 @@ public class UserControllerTest {
 		UserDto userDto = createUserDto();
 		userDto.setRoleId(1);
 		userDto.setEmail("tuanlahe141277@fpt.edu.vn");
-		this.mockMvc.perform(put("/api/admin/hr/updateuser/{studentId}", "HE140855")
+		this.mockMvc.perform(put("/api/admin/hr/updateuser/{studentId}", "HE10001")
 		.content(asJsonString(userDto))
 		.contentType(MediaType.APPLICATION_JSON))
 		.andExpect(status().isOk())
@@ -215,16 +265,6 @@ public class UserControllerTest {
 		userDto.setEmail("toandam10@fpt.edu.vn");
 		this.mockMvc.perform(post("/api/admin/hr/adduser")
 		.content(asJsonString(userDto))
-		.contentType(MediaType.APPLICATION_JSON))
-		.andExpect(status().isOk())
-		.andExpect(content()
-				.contentType(MediaType.APPLICATION_JSON))
-		.andExpect(jsonPath("$.data.size()").value(1));
-	}
-	
-	@Test
-	public void deleteAdminTestSuccess() throws Exception {
-		this.mockMvc.perform(put("/api/admin/hr/deleteadmin/{studentId}", "HE140855").param("semester", "Summer2022")
 		.contentType(MediaType.APPLICATION_JSON))
 		.andExpect(status().isOk())
 		.andExpect(content()
@@ -342,6 +382,78 @@ public class UserControllerTest {
 		.andExpect(jsonPath("$.data.size()").value(3));
 	}
 	
+	@Test
+	public void testGetAdminsBySemesterSuccess() throws Exception{
+		this.mockMvc.perform(get("/api/admin/hr/viceheadclub/getadmins/semester").param("semester", "Summer2022")).andExpect(status().isOk())
+		.andExpect(content().contentType(MediaType.APPLICATION_JSON))
+		.andExpect(jsonPath("$.data.size()").value(4));
+	}
+	
+	@Test
+	public void testSearchSuccess() throws Exception{
+		List<UserDto> users = usersDto();
+		this.mockMvc.perform(post("/api/admin/hr/viceheadclub/member/search").param("name", "toan").content(asJsonString(users))
+				.contentType(MediaType.APPLICATION_JSON)).andExpect(status().isOk())
+		.andExpect(jsonPath("$.data.size()").value(2));
+	}
+	
+	@Test
+	public void testSearchStudentIdSuccess() throws Exception{
+		List<UserDto> users = usersDto();
+		this.mockMvc.perform(post("/api/admin/hr/viceheadclub/member/search").param("studentId", "HE").content(asJsonString(users))
+				.contentType(MediaType.APPLICATION_JSON)).andExpect(status().isOk())
+		.andExpect(jsonPath("$.data.size()").value(2));
+	}
+	
+	@Test
+	public void testSearchEmailSuccess() throws Exception{
+		List<UserDto> users = usersDto();
+		this.mockMvc.perform(post("/api/admin/hr/viceheadclub/member/search").param("email", "toandv").content(asJsonString(users))
+				.contentType(MediaType.APPLICATION_JSON)).andExpect(status().isOk())
+		.andExpect(jsonPath("$.data.size()").value(2));
+	}
+	@Test
+	public void testSearchGenderSuccess() throws Exception{
+		List<UserDto> users = usersDto();
+		this.mockMvc.perform(post("/api/admin/hr/viceheadclub/member/search").param("gender", "true").content(asJsonString(users))
+				.contentType(MediaType.APPLICATION_JSON)).andExpect(status().isOk())
+		.andExpect(jsonPath("$.data.size()").value(2));
+	}
+	
+	@Test
+	public void testSearchGenerationSuccess() throws Exception{
+		List<UserDto> users = usersDto();
+		this.mockMvc.perform(post("/api/admin/hr/viceheadclub/member/search").param("generation", "4").content(asJsonString(users))
+				.contentType(MediaType.APPLICATION_JSON)).andExpect(status().isOk())
+		.andExpect(jsonPath("$.data.size()").value(2));
+	}
+	
+	@Test
+	public void testSearchRoleIdSuccess() throws Exception{
+		List<UserDto> users = usersDto();
+		this.mockMvc.perform(post("/api/admin/hr/viceheadclub/member/search").param("roleId", "1").content(asJsonString(users))
+				.contentType(MediaType.APPLICATION_JSON)).andExpect(status().isOk())
+		.andExpect(jsonPath("$.data.size()").value(1));
+	}
+	
+	@Test
+	public void testSearchActiveSuccess() throws Exception{
+		List<UserDto> users = usersDto();
+		this.mockMvc.perform(post("/api/admin/hr/viceheadclub/member/search").param("isActive", "true").content(asJsonString(users))
+				.contentType(MediaType.APPLICATION_JSON)).andExpect(status().isOk())
+		.andExpect(jsonPath("$.data.size()").value(2));
+	}
+	
+	@Test
+	public void testGenerateQrCode() throws Exception{
+		InforInQrCode inforInQrCode = new InforInQrCode();
+		inforInQrCode.setStudentId("HE140855");
+		inforInQrCode.setStudentName("dam van toan");
+		inforInQrCode.setEmail(null);
+		this.mockMvc.perform(post("/api/admin/hr/member/qrcode/create").content(asJsonString(inforInQrCode))
+				.contentType(MediaType.APPLICATION_JSON)).andExpect(status().isOk())
+		.andExpect(jsonPath("$.data.size()").value(1));
+	}
 	public static String asJsonString(final Object obj) {
 	    try {
 	      return new ObjectMapper().writeValueAsString(obj);
