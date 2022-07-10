4c811e9ac2421a298b8d82b0216c24533fad939d
DESKTOP-RJUK2FF\VAN TOAN <toandam0210@gmail.com>
2022-07-10 23:38:31 +0700

    oauth2 login

--- src/main/java/com/fpt/macm/controller/LoginController.java
+++ src/main/java/com/fpt/macm/controller/LoginController.java
@@ -1,24 +1,17 @@
-//package com.fpt.macm.controller;
-//
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.http.HttpStatus;
-//import org.springframework.http.ResponseEntity;
-//import org.springframework.web.bind.annotation.GetMapping;
-//import org.springframework.web.bind.annotation.RequestMapping;
-//import org.springframework.web.bind.annotation.RestController;
-//
-//import com.fpt.macm.model.ResponseMessage;
-//import com.fpt.macm.service.UserService;
-//
-//@RestController
-//@RequestMapping("/api")
-//public class LoginController {
-//	
-//	@Autowired
-//	UserService userService;
-//	
-//	@GetMapping("/login")
-//	ResponseEntity<ResponseMessage> getUserLogin(){
-//		return new ResponseEntity<ResponseMessage>(userService.userLogin(), HttpStatus.OK);
-//	}
-//}
+package com.fpt.macm.controller;
+
+import java.security.Principal;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
+public class LoginController {
+	
+	@GetMapping("/user")
+	public Principal getUserLogin(Principal principal){
+		return principal;
+	}
+}
