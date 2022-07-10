bb48731cb7aea9985c7aecc2e4925e803463e32d
Pham Minh Duc <menly10x@gmail.com>
2022-07-11 03:54:52 +0700

    fee dashboard

--- src/main/java/com/fpt/macm/repository/TournamentOrganizingCommitteePaymentStatusReportRepository.java
+++ src/main/java/com/fpt/macm/repository/TournamentOrganizingCommitteePaymentStatusReportRepository.java
@@ -1,8 +1,10 @@
 package com.fpt.macm.repository;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.fpt.macm.model.TournamentOrganizingCommitteePaymentStatusReport;
@@ -12,5 +14,8 @@ public interface TournamentOrganizingCommitteePaymentStatusReportRepository
 		extends JpaRepository<TournamentOrganizingCommitteePaymentStatusReport, Integer> {
 
 	List<TournamentOrganizingCommitteePaymentStatusReport> findByTournamentId(int tournamentId);
+	
+	@Query(value = "SELECT * FROM tournament_organizing_committee_payment_status_report WHERE created_on BETWEEN ?1 AND ?2", nativeQuery = true)
+	List<TournamentOrganizingCommitteePaymentStatusReport> findAllFundChange(LocalDateTime startDate, LocalDateTime endDate);
 	
 }
--- src/main/java/com/fpt/macm/service/DashboardServiceImpl.java
+++ src/main/java/com/fpt/macm/service/DashboardServiceImpl.java
@@ -1,6 +1,7 @@
 package com.fpt.macm.service;
 
 import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
@@ -10,54 +11,79 @@ import org.springframework.stereotype.Service;
 
 import com.fpt.macm.dto.AttendanceReportDto;
 import com.fpt.macm.dto.EventDashboardDto;
+import com.fpt.macm.dto.FeeDashboardDto;
 import com.fpt.macm.model.AttendanceStatus;
+import com.fpt.macm.model.ClubFundReport;
 import com.fpt.macm.model.CollaboratorReport;
 import com.fpt.macm.model.Event;
+import com.fpt.macm.model.EventPaymentStatusReport;
 import com.fpt.macm.model.MemberEvent;
+import com.fpt.macm.model.MembershipPaymentStatusReport;
 import com.fpt.macm.model.ResponseMessage;
 import com.fpt.macm.model.Semester;
+import com.fpt.macm.model.TournamentOrganizingCommitteePaymentStatusReport;
+import com.fpt.macm.model.TournamentPlayerPaymentStatusReport;
 import com.fpt.macm.model.TrainingSchedule;
 import com.fpt.macm.model.UserStatusReport;
 import com.fpt.macm.repository.AttendanceStatusRepository;
+import com.fpt.macm.repository.ClubFundReportRepository;
 import com.fpt.macm.repository.CollaboratorReportRepository;
+import com.fpt.macm.repository.EventPaymentStatusReportRepository;
 import com.fpt.macm.repository.EventRepository;
 import com.fpt.macm.repository.MemberEventRepository;
+import com.fpt.macm.repository.MembershipPaymentStatusReportRepository;
 import com.fpt.macm.repository.SemesterRepository;
+import com.fpt.macm.repository.TournamentOrganizingCommitteePaymentStatusReportRepository;
+import com.fpt.macm.repository.TournamentPlayerPaymentStatusReportRepository;
 import com.fpt.macm.repository.TournamentRepository;
 import com.fpt.macm.repository.TrainingScheduleRepository;
 import com.fpt.macm.repository.UserStatusReportRepository;
 
 @Service
 public class DashboardServiceImpl implements DashboardService {
-	
+
 	@Autowired
 	CollaboratorReportRepository collaboratorReportRepository;
-	
+
 	@Autowired
 	SemesterRepository semesterRepository;
-	
+
 	@Autowired
 	AttendanceStatusRepository attendanceStatusRepository;
-	
+
 	@Autowired
 	TrainingScheduleRepository trainingScheduleRepository;
-	
+
 	@Autowired
 	EventRepository eventRepository;
-	
+
 	@Autowired
 	TournamentRepository tournamentRepository;
-	
+
 	@Autowired
 	MemberEventRepository memberEventRepository;
-	
+
 	@Autowired
 	EventService eventService;
-	
+
 	@Autowired
 	UserStatusReportRepository userStatusReportRepository;
-	
-	
+
+	@Autowired
+	ClubFundReportRepository clubFundReportRepository;
+
+	@Autowired
+	MembershipPaymentStatusReportRepository membershipPaymentStatusReportRepository;
+
+	@Autowired
+	EventPaymentStatusReportRepository eventPaymentStatusReportRepository;
+
+	@Autowired
+	TournamentOrganizingCommitteePaymentStatusReportRepository tournamentOrganizingCommitteePaymentStatusReportRepository;
+
+	@Autowired
+	TournamentPlayerPaymentStatusReportRepository tournamentPlayerPaymentStatusReportRepository;
+
 	@Override
 	public ResponseMessage getCollaboratorReport() {
 		ResponseMessage responseMessage = new ResponseMessage();
@@ -71,7 +97,7 @@ public class DashboardServiceImpl implements DashboardService {
 		}
 		return responseMessage;
 	}
-	
+
 	@Override
 	public ResponseMessage attendanceReport(String semesterName) {
 		ResponseMessage responseMessage = new ResponseMessage();
@@ -92,10 +118,10 @@ public class DashboardServiceImpl implements DashboardService {
 					for (AttendanceStatus attendanceStatus : listAttendanceStatus) {
 						if (trainingSchedule.getId() == attendanceStatus.getTrainingSchedule().getId()) {
 							totalUserJoinTrainingSession++;
-							if(attendanceStatus.getStatus() == 1) {
+							if (attendanceStatus.getStatus() == 1) {
 								totalUserAttentInTrainingSession++;
 							}
-							if(attendanceStatus.getStatus() == 0) {
+							if (attendanceStatus.getStatus() == 0) {
 								totalUserAbsentInTrainingSession++;
 							}
 						}
@@ -107,7 +133,7 @@ public class DashboardServiceImpl implements DashboardService {
 				}
 				List<AttendanceReportDto> listRemove = new ArrayList<AttendanceReportDto>();
 				for (AttendanceReportDto attendanceReportDto : attendanceReportDtos) {
-					if(attendanceReportDto.getDate().isAfter(LocalDate.now())) {
+					if (attendanceReportDto.getDate().isAfter(LocalDate.now())) {
 						listRemove.add(attendanceReportDto);
 					}
 				}
@@ -130,14 +156,14 @@ public class DashboardServiceImpl implements DashboardService {
 			List<EventDashboardDto> eventDashboardDtos = new ArrayList<EventDashboardDto>();
 			for (Event event : events) {
 				LocalDate startDate = eventService.getStartDate(event.getId());
-				if(startDate.isBefore(LocalDate.now())) {
-				List<MemberEvent> memberEvents = memberEventRepository.findByEventIdOrderByIdAsc(event.getId());
-				EventDashboardDto eventDashboardDto = new EventDashboardDto();
-				eventDashboardDto.setTotalMemberJoin(memberEvents.size());
-				eventDashboardDto.setTotalRevenueInEvent(event.getTotalAmountActual());
-				eventDashboardDto.setTotalExpenditure(event.getAmountPerRegisterActual() * memberEvents.size());
-				eventDashboardDto.setEventName(event.getName());
-				eventDashboardDtos.add(eventDashboardDto);
+				if (startDate.isBefore(LocalDate.now())) {
+					List<MemberEvent> memberEvents = memberEventRepository.findByEventIdOrderByIdAsc(event.getId());
+					EventDashboardDto eventDashboardDto = new EventDashboardDto();
+					eventDashboardDto.setTotalMemberJoin(memberEvents.size());
+					eventDashboardDto.setTotalRevenueInEvent(event.getTotalAmountActual());
+					eventDashboardDto.setTotalExpenditure(event.getAmountPerRegisterActual() * memberEvents.size());
+					eventDashboardDto.setEventName(event.getName());
+					eventDashboardDtos.add(eventDashboardDto);
 				}
 			}
 			responseMessage.setData(eventDashboardDtos);
@@ -154,12 +180,97 @@ public class DashboardServiceImpl implements DashboardService {
 		ResponseMessage responseMessage = new ResponseMessage();
 		try {
 			List<UserStatusReport> userStatusReports = userStatusReportRepository.findAll();
-			if(userStatusReports.size() > 0) {
+			if (userStatusReports.size() > 0) {
 				responseMessage.setData(userStatusReports);
 				responseMessage.setMessage("Lấy dữ liệu report thành công");
 				responseMessage.setTotalResult(userStatusReports.size());
-			}else {
+			} else {
 				responseMessage.setMessage("Không có dữ liệu");
+			}
+		} catch (Exception e) {
+			responseMessage.setMessage(e.getMessage());
+		}
+		return responseMessage;
+	}
+
+	@Override
+	public ResponseMessage feeReport(String semesterName) {
+		ResponseMessage responseMessage = new ResponseMessage();
+		try {
+			Optional<Semester> semesterOp = semesterRepository.findByName(semesterName);
+			if (semesterOp.isPresent()) {
+				Semester semester = semesterOp.get();
+
+				int startMonth = semester.getStartDate().getMonthValue();
+				int endMonth = semester.getEndDate().getMonthValue();
+				if (endMonth == 1) {
+					endMonth = 13;
+				}
+				int year = semester.getStartDate().getYear();
+
+				List<FeeDashboardDto> feeDashboardsDto = new ArrayList<FeeDashboardDto>();
+				
+				for (int i = startMonth; i < endMonth; i++) {
+					int totalIncome = 0;
+					int totalSpend = 0;
+					
+					LocalDateTime startDate = LocalDateTime.of(year, i, 1, 0, 0, 0);
+					LocalDate startDateTemp = startDate.toLocalDate();
+					LocalDateTime endDate = LocalDateTime.of(year, i,
+							startDateTemp.withDayOfMonth(startDateTemp.getMonth().length(startDateTemp.isLeapYear()))
+									.getDayOfMonth(),
+							23, 59, 59);
+
+					List<ClubFundReport> clubFundReports = clubFundReportRepository.findAllFundChange(startDate,
+							endDate);
+					for (ClubFundReport clubFundReport : clubFundReports) {
+						double fundChange = clubFundReport.getFundChange();
+						if (fundChange < 0) {
+							totalSpend += -fundChange;
+						} else {
+							totalIncome += fundChange;
+						}
+					}
+					
+					List<MembershipPaymentStatusReport> membershipPaymentStatusReports = membershipPaymentStatusReportRepository
+							.findAllFundChange(startDate, endDate);
+					for (MembershipPaymentStatusReport membershipPaymentStatusReport : membershipPaymentStatusReports) {
+						double fundChange = membershipPaymentStatusReport.getFundChange();
+						totalIncome += fundChange;
+					}
+					
+					List<EventPaymentStatusReport> eventPaymentStatusReports = eventPaymentStatusReportRepository
+							.findAllFundChange(startDate, endDate);
+					for (EventPaymentStatusReport eventPaymentStatusReport : eventPaymentStatusReports) {
+						double fundChange = eventPaymentStatusReport.getFundChange();
+						totalIncome += fundChange;
+					}
+					
+					List<TournamentOrganizingCommitteePaymentStatusReport> tournamentOrganizingCommitteePaymentStatusReports = tournamentOrganizingCommitteePaymentStatusReportRepository
+							.findAllFundChange(startDate, endDate);
+					for (TournamentOrganizingCommitteePaymentStatusReport tournamentOrganizingCommitteePaymentStatusReport : tournamentOrganizingCommitteePaymentStatusReports) {
+						double fundChange = tournamentOrganizingCommitteePaymentStatusReport.getFundChange();
+						totalIncome += fundChange;
+					}
+					
+					List<TournamentPlayerPaymentStatusReport> tournamentPlayerPaymentStatusReports = tournamentPlayerPaymentStatusReportRepository
+							.findAllFundChange(startDate, endDate);
+					for (TournamentPlayerPaymentStatusReport tournamentPlayerPaymentStatusReport : tournamentPlayerPaymentStatusReports) {
+						double fundChange = tournamentPlayerPaymentStatusReport.getFundChange();
+						totalIncome += fundChange;
+					}
+					
+					FeeDashboardDto feeDashboardDto = new FeeDashboardDto();
+					feeDashboardDto.setSemester(semesterName);
+					feeDashboardDto.setMonth(i);
+					feeDashboardDto.setTotalIncome(totalIncome);
+					feeDashboardDto.setTotalSpend(totalSpend);
+					
+					feeDashboardsDto.add(feeDashboardDto);
+				}
+				
+				responseMessage.setData(feeDashboardsDto);
+				responseMessage.setMessage("Lấy dữ liệu báo cáo thu chi theo kỳ thành công");
 			}
 		} catch (Exception e) {
 			responseMessage.setMessage(e.getMessage());
