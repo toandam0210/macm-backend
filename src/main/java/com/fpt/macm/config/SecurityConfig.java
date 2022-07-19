128d2b6f23a6102679557a36d028906bfd0c0e59
toandam0210 <84827568+toandam0210@users.noreply.github.com>
2022-07-19 21:59:17 +0700

    Feature/login oauth2 v6 (#281)
    * oauth2 login with google
    
    * no message
    
    * no message
    
    * login oauth2 with google
    
    * fix login
    
    * no message
    
    * phân quyền
    
    * phân quyền
    
    * config file security
    
    * fix swagger config
    
    Co-authored-by: Pham Minh Duc <menly10x@gmail.com>

--- src/main/java/com/fpt/macm/config/SecurityConfig.java
+++ src/main/java/com/fpt/macm/config/SecurityConfig.java
@@ -5,6 +5,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
@@ -54,6 +55,16 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Bean
     public HttpCookieOAuth2AuthorizationRequestRepository cookieAuthorizationRequestRepository() {
         return new HttpCookieOAuth2AuthorizationRequestRepository();
+    }
+    
+    @Override
+    public void configure(WebSecurity web) throws Exception {
+        web.ignoring().antMatchers("/v2/api-docs",
+                                   "/configuration/ui",
+                                   "/swagger-resources/**",
+                                   "/configuration/security",
+                                   "/swagger-ui/**",
+                                   "/webjars/**");
     }
 
     @Override
