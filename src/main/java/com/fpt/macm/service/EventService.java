7b469331b86ad0e776a260fad3959db6ad57f4e3
Pham Minh Duc <73643261+menly10x@users.noreply.github.com>
2022-07-28 14:53:52 +0700

    get all event by semester and studentId (#289)

--- src/main/java/com/fpt/macm/service/EventService.java
+++ src/main/java/com/fpt/macm/service/EventService.java
@@ -16,4 +16,5 @@ public interface EventService {
 	ResponseMessage getEventsBySemester(String semester, int month, int pageNo, int pageSize);
 	ResponseMessage updateAfterEvent(int eventId, double money, boolean isIncurred, boolean isUseClubFund);
 	LocalDate getStartDate(int eventId);
+	ResponseMessage getEventsBySemesterAndStudentId(String semester, String studentId, int month, int pageNo, int pageSize);
 }
