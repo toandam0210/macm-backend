5f1f006f68e812d3840c9355ee0a2a090125f176
LinhLHN <65857792+LinhLHN@users.noreply.github.com>
2022-08-01 23:26:45 +0700

    fix register team by admin (#326)
    * fix list match
    
    * fix register exhibition team by admin

--- src/main/java/com/fpt/macm/repository/ExhibitionPlayerRepository.java
+++ src/main/java/com/fpt/macm/repository/ExhibitionPlayerRepository.java
@@ -11,7 +11,7 @@ import com.fpt.macm.model.entity.ExhibitionPlayer;
 
 @Repository
 public interface ExhibitionPlayerRepository extends JpaRepository<ExhibitionPlayer, Integer>{
-	@Query(value = "select exhibition_player.id, exhibition_player.role_in_team , exhibition_player.player_id, exhibition_player.exhibition_team_id \r\n"
+	@Query(value = "select * \r\n"
 			+ "from exhibition_player inner join exhibition_team on exhibition_player.exhibition_team_id = exhibition_team.id  \r\n"
 			+ "where player_id = ?1 and exhibition_type_id = ?2", nativeQuery = true)
 	Optional<ExhibitionPlayer> findByTournamentPlayerAndType(int tournamentPlayerId, int exhibitionTypeId);
