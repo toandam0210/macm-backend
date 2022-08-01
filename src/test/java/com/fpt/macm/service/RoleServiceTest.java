00ec7dc9297b99d645eb04e65d310c10f5abcd1f
Pham Minh Duc <menly10x@gmail.com>
2022-08-01 22:42:57 +0700

    unit test for roleServiceImpl

---
+++ src/test/java/com/fpt/macm/service/RoleServiceTest.java
@@ -0,0 +1,64 @@
+package com.fpt.macm.service;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.when;
+
+import java.util.Arrays;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import com.fpt.macm.model.entity.Role;
+import com.fpt.macm.model.response.ResponseMessage;
+import com.fpt.macm.repository.RoleRepository;
+
+@ExtendWith(MockitoExtension.class)
+public class RoleServiceTest {
+
+	@InjectMocks
+	RoleService roleService = new RoleServiceImpl();
+	
+	@Mock
+	RoleRepository roleRepository;
+	
+	private Role role() {
+		Role role = new Role();
+		role.setId(1);
+		role.setName("Head_Club");
+		return role;
+	}
+	
+	@Test
+	public void addListRoleCaseSuccess() {
+		ResponseMessage responseMessage = roleService.addListRole(Arrays.asList(role()));
+		assertEquals(responseMessage.getData().size(), 1);
+	}
+	
+	@Test
+	public void getAllRoleCaseSuccess() {
+		when(roleRepository.findAll()).thenReturn(Arrays.asList(role()));
+		
+		ResponseMessage responseMessage = roleService.getAllRole();
+		assertEquals(responseMessage.getData().size(), 1);
+	}
+	
+	@Test
+	public void getAllRoleCaseException() {
+		when(roleRepository.findAll()).thenReturn(null);
+		
+		ResponseMessage responseMessage = roleService.getAllRole();
+		assertEquals(responseMessage.getData().size(), 0);
+	}
+	
+	@Test
+	public void getRoleForViceHeadClubCaseSuccess() {
+		when(roleRepository.findRoleForViceHead()).thenReturn(Arrays.asList(role()));
+		
+		ResponseMessage responseMessage = roleService.getRoleForViceHeadClub();
+		assertEquals(responseMessage.getData().size(), 1);
+	}
+	
+}
