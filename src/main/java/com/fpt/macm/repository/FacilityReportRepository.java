77133bfb0342841d14854e13cc41c846e7ad9f20
Pham Minh Duc <menly10x@gmail.com>
2022-08-01 22:54:46 +0700

    xóa facility với news

--- src/main/java/com/fpt/macm/repository/FacilityReportRepository.java
+++
@@ -1,9 +0,0 @@
-package com.fpt.macm.repository;
-
-import org.springframework.data.repository.PagingAndSortingRepository;
-
-import com.fpt.macm.model.entity.FacilityReport;
-
-public interface FacilityReportRepository extends PagingAndSortingRepository<FacilityReport, Integer>{
-
-}
