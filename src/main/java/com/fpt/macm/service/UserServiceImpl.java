4e00cf8a153ff62a28d8ddf563b5ae71e3b03e8d
toandam0210 <84827568+toandam0210@users.noreply.github.com>
2022-08-02 23:51:55 +0700

    fix export excel (#330)

--- src/main/java/com/fpt/macm/service/UserServiceImpl.java
+++ src/main/java/com/fpt/macm/service/UserServiceImpl.java
@@ -478,8 +478,7 @@ public class UserServiceImpl implements UserService {
 		}
 	}
 
-	public ByteArrayInputStream exportUsersToExcel() {
-		List<User> users = (List<User>) userRepository.findAll();
+	public ByteArrayInputStream exportUsersToExcel(List<User> users) {
 		ByteArrayInputStream in = ExcelHelper.usersToExcel(users);
 		return in;
 	}
