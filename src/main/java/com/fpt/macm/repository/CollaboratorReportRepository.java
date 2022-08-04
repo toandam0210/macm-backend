79a25c69588ef2980713090af0926b14890e6cb2
toandam0210 <84827568+toandam0210@users.noreply.github.com>
2022-08-05 00:41:47 +0700

    Fix/security config (#360)
    * fix authorization
    
    * fix update role collaborator

--- src/main/java/com/fpt/macm/repository/CollaboratorReportRepository.java
+++ src/main/java/com/fpt/macm/repository/CollaboratorReportRepository.java
@@ -1,5 +1,7 @@
 package com.fpt.macm.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -7,5 +9,7 @@ import com.fpt.macm.model.entity.CollaboratorReport;
 
 @Repository
 public interface CollaboratorReportRepository extends JpaRepository<CollaboratorReport, Integer> {
+	
+	Optional<CollaboratorReport> findBySemester(String semester);
 
 }
