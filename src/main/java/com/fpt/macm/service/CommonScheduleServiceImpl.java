d0aae4a5654f2bc41a6b07caf05ac47737c6e33f
Pham Minh Duc <73643261+menly10x@users.noreply.github.com>
2022-08-11 23:27:59 +0700

    Feature/upcoming event (#429)
    * thêm danh sách hoạt động sắp tới
    
    * fix v1
    
    * fix v2

--- src/main/java/com/fpt/macm/service/CommonScheduleServiceImpl.java
+++ src/main/java/com/fpt/macm/service/CommonScheduleServiceImpl.java
@@ -1,9 +1,7 @@
 package com.fpt.macm.service;
 
 import java.time.LocalDate;
-import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Collections;
 import java.util.List;
 import java.util.Optional;
 
@@ -11,11 +9,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.fpt.macm.constant.Constant;
-import com.fpt.macm.model.dto.UpcomingActivityDto;
 import com.fpt.macm.model.entity.CommonSchedule;
-import com.fpt.macm.model.entity.Event;
 import com.fpt.macm.model.entity.Semester;
-import com.fpt.macm.model.entity.Tournament;
 import com.fpt.macm.model.response.ResponseMessage;
 import com.fpt.macm.repository.CommonScheduleRepository;
 import com.fpt.macm.repository.EventRepository;
@@ -105,51 +100,51 @@ public class CommonScheduleServiceImpl implements CommonScheduleService {
 		return responseMessage;
 	}
 
-	@Override
-	public ResponseMessage getAllUpcomingActivities(String semesterName) {
-		ResponseMessage responseMessage = new ResponseMessage();
-		try {
-			List<UpcomingActivityDto> upcomingActivitiesDto = new ArrayList<UpcomingActivityDto>();
-			List<Event> events = eventRepository.findBySemesterOrderByIdAsc(semesterName);
-			if (!events.isEmpty()) {
-				for (Event event : events) {
-					LocalDate startDate = eventService.getStartDate(event.getId());
-					if (startDate != null && startDate.isAfter(LocalDate.now())) {
-						UpcomingActivityDto upcomingActivityDto = new UpcomingActivityDto();
-						upcomingActivityDto.setId(event.getId());
-						upcomingActivityDto.setName(event.getName());
-						upcomingActivityDto.setDate(startDate);
-						upcomingActivityDto.setType(1);
-						upcomingActivitiesDto.add(upcomingActivityDto);
-					}
-				}
-			}
-			
-			List<Tournament> tournaments = tournamentRepository.findBySemester(semesterName);
-			if (!tournaments.isEmpty()) {
-				for (Tournament tournament : tournaments) {
-					LocalDate startDate = tournamentService.getStartDate(tournament.getId());
-					if (startDate != null && startDate.isAfter(LocalDate.now())) {
-						UpcomingActivityDto upcomingActivityDto = new UpcomingActivityDto();
-						upcomingActivityDto.setId(tournament.getId());
-						upcomingActivityDto.setName(tournament.getName());
-						upcomingActivityDto.setDate(startDate);
-						upcomingActivityDto.setType(2);
-						upcomingActivitiesDto.add(upcomingActivityDto);
-					}
-				}
-			}
-			
-			if (!upcomingActivitiesDto.isEmpty()) {
-				Collections.sort(upcomingActivitiesDto);
-				responseMessage.setData(upcomingActivitiesDto);
-				responseMessage.setMessage("Lấy hoạt động sắp tới thành công");
-			} else {
-				responseMessage.setMessage("Sắp tới không có hoạt động nào");
-			}
-		} catch (Exception e) {
-			responseMessage.setMessage(e.getMessage());
-		}
-		return responseMessage;
-	}
+//	@Override
+//	public ResponseMessage getAllUpcomingActivities(String semesterName) {
+//		ResponseMessage responseMessage = new ResponseMessage();
+//		try {
+//			List<UpcomingActivityDto> upcomingActivitiesDto = new ArrayList<UpcomingActivityDto>();
+//			List<Event> events = eventRepository.findBySemesterOrderByIdAsc(semesterName);
+//			if (!events.isEmpty()) {
+//				for (Event event : events) {
+//					LocalDate startDate = eventService.getStartDate(event.getId());
+//					if (startDate != null && startDate.isAfter(LocalDate.now())) {
+//						UpcomingActivityDto upcomingActivityDto = new UpcomingActivityDto();
+//						upcomingActivityDto.setId(event.getId());
+//						upcomingActivityDto.setName(event.getName());
+//						upcomingActivityDto.setDate(startDate);
+//						upcomingActivityDto.setType(1);
+//						upcomingActivitiesDto.add(upcomingActivityDto);
+//					}
+//				}
+//			}
+//			
+//			List<Tournament> tournaments = tournamentRepository.findBySemester(semesterName);
+//			if (!tournaments.isEmpty()) {
+//				for (Tournament tournament : tournaments) {
+//					LocalDate startDate = tournamentService.getStartDate(tournament.getId());
+//					if (startDate != null && startDate.isAfter(LocalDate.now())) {
+//						UpcomingActivityDto upcomingActivityDto = new UpcomingActivityDto();
+//						upcomingActivityDto.setId(tournament.getId());
+//						upcomingActivityDto.setName(tournament.getName());
+//						upcomingActivityDto.setDate(startDate);
+//						upcomingActivityDto.setType(2);
+//						upcomingActivitiesDto.add(upcomingActivityDto);
+//					}
+//				}
+//			}
+//			
+//			if (!upcomingActivitiesDto.isEmpty()) {
+//				Collections.sort(upcomingActivitiesDto);
+//				responseMessage.setData(upcomingActivitiesDto);
+//				responseMessage.setMessage("Lấy hoạt động sắp tới thành công");
+//			} else {
+//				responseMessage.setMessage("Sắp tới không có hoạt động nào");
+//			}
+//		} catch (Exception e) {
+//			responseMessage.setMessage(e.getMessage());
+//		}
+//		return responseMessage;
+//	}
 }
