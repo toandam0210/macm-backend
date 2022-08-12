04afa30a3515823d251283569a22e5c112dd4fd4
Pham Minh Duc <73643261+menly10x@users.noreply.github.com>
2022-08-12 11:23:39 +0700

    thêm số cặp đấu (#450)

--- src/main/java/com/fpt/macm/model/dto/CompetitiveMatchDto.java
+++ src/main/java/com/fpt/macm/model/dto/CompetitiveMatchDto.java
@@ -10,32 +10,36 @@ public class CompetitiveMatchDto implements Comparable<CompetitiveMatchDto> {
 	private boolean status;
 	private PlayerMatchDto firstPlayer;
 	private PlayerMatchDto secondPlayer;
-	
+	private int matchNo;
+
 	public int getId() {
 		return id;
 	}
+
 	public void setId(int id) {
 		this.id = id;
 	}
+
 	public int getRound() {
 		return round;
 	}
+
 	public void setRound(int round) {
 		this.round = round;
 	}
-	
+
 	public LocalDateTime getTime() {
 		return time;
 	}
-	
+
 	public void setTime(LocalDateTime time) {
 		this.time = time;
 	}
-	
+
 	public String getArea() {
 		return area;
 	}
-	
+
 	public void setArea(String area) {
 		this.area = area;
 	}
@@ -43,32 +47,39 @@ public class CompetitiveMatchDto implements Comparable<CompetitiveMatchDto> {
 	public boolean getStatus() {
 		return status;
 	}
-	
+
 	public void setStatus(boolean status) {
 		this.status = status;
 	}
-	
+
 	public PlayerMatchDto getFirstPlayer() {
 		return firstPlayer;
 	}
-	
+
 	public void setFirstPlayer(PlayerMatchDto firstPlayer) {
 		this.firstPlayer = firstPlayer;
 	}
-	
+
 	public PlayerMatchDto getSecondPlayer() {
 		return secondPlayer;
 	}
-	
+
 	public void setSecondPlayer(PlayerMatchDto secondPlayer) {
 		this.secondPlayer = secondPlayer;
 	}
-	
+
 	@Override
 	public int compareTo(CompetitiveMatchDto o) {
 		// TODO Auto-generated method stub
 		return this.getId() - o.getId();
 	}
-	
-	
+
+	public int getMatchNo() {
+		return matchNo;
+	}
+
+	public void setMatchNo(int matchNo) {
+		this.matchNo = matchNo;
+	}
+
 }
