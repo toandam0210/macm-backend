560bdf4c4dd42c6e7ee43038ef8834b71828d1e1
toandam0210 <84827568+toandam0210@users.noreply.github.com>
2022-08-16 02:03:01 +0700

    fix export excel break line message error

--- src/main/java/com/fpt/macm/service/UserServiceImpl.java
+++ src/main/java/com/fpt/macm/service/UserServiceImpl.java
@@ -1090,7 +1090,7 @@ public class UserServiceImpl implements UserService {
 
 			responseMessage.setData(listUserAttendanceStatusDto);
 			responseMessage.setMessage(
-					"Lấy dữ liệu điểm danh của " + user.getName() + " - " + user.getStudentId() + "<br>" +" thành công");
+					"Lấy dữ liệu điểm danh của " + user.getName() + " - " + user.getStudentId() + " thành công");
 		} catch (Exception e) {
 			// TODO: handle exception
 			responseMessage.setMessage(e.getMessage());
@@ -1197,8 +1197,4 @@ public class UserServiceImpl implements UserService {
 		return responseMessage;
 	}
 	
-	public static void main(String[] args) {
-		System.out.println("a" + System.lineSeparator() + "b");
-	}
-
 }
