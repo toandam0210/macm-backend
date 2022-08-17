368efd94f0d54aaad2d89b1b6073bc2b18f34dc3
toandam0210 <84827568+toandam0210@users.noreply.github.com>
2022-08-18 00:46:16 +0700

    Fix/create tournament (#515)
    * fix create user
    
    * fix create user
    
    * fix update user
    
    * fix unit test

--- src/main/java/com/fpt/macm/service/UserServiceImpl.java
+++ src/main/java/com/fpt/macm/service/UserServiceImpl.java
@@ -183,7 +183,7 @@ public class UserServiceImpl implements UserService {
 			boolean checkDuplicateStudentId = false;
 			Role currentUserRole = user.getRole();
 			Optional<Role> roleOptional = roleRepository.findById(userDto.getRoleId());
-			if (currentUserRole.getId() > 12 && currentUserRole.getId() < 15
+			if (currentUserRole.getId() > 12 && currentUserRole.getId() < 16
 					&& roleOptional.get().getId() > 0 && roleOptional.get().getId() < 10) {
 				responseMessage.setMessage("CTV không thể cập nhật lên làm ban chủ nhiệm");
 			} else if(currentUserRole.getName().equals(ERole.ROLE_HeadClub.name()) && userDto.getRoleId() > 1 && userDto.getRoleId() < 10){
