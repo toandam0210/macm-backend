2afe6d4e9222b54f32b3b862fed73292f15eb13f
LinhLHN <65857792+LinhLHN@users.noreply.github.com>
2022-08-24 04:37:22 +0700

    fix add competitive player (#568)
    * setting semester, setting area
    
    * fix get result of competitive
    
    * fix add competitive player

--- src/main/java/com/fpt/macm/repository/AreaRepository.java
+++ src/main/java/com/fpt/macm/repository/AreaRepository.java
@@ -1,11 +1,15 @@
 package com.fpt.macm.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.fpt.macm.model.entity.Area;
 
 @Repository
 public interface AreaRepository extends JpaRepository<Area, Integer>{
-
+	@Query(value = "select * from area where is_active = true", nativeQuery = true)
+	List<Area> listActiveArea();
 }
