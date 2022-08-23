be4e5355307586420ae3f4ef48eea1b1b973d559
Pham Minh Duc <73643261+menly10x@users.noreply.github.com>
2022-08-24 01:43:44 +0700

    fix get month of semester (#559)

--- src/main/java/com/fpt/macm/service/SemesterServiceImpl.java
+++ src/main/java/com/fpt/macm/service/SemesterServiceImpl.java
@@ -4,6 +4,7 @@ import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -56,26 +57,35 @@ public class SemesterServiceImpl implements SemesterService {
 	}
 
 	@Override
-	public ResponseMessage getListMonthsBySemester(String semester) {
+	public ResponseMessage getListMonthsBySemester(String semesterName) {
 		// TODO Auto-generated method stub
 		ResponseMessage responseMessage = new ResponseMessage();
 		try {
-			if(semester == "") {
-				semester = semesterRepository.findTop3Semester().get(0).getName();
-			}
+//			if(semester == "") {
+//				semester = semesterRepository.findTop3Semester().get(0).getName();
+//			}
 			List<Integer> listMonth = new ArrayList<Integer>();
-			if(semester.contains("Spring")) {
-				Integer [] list = {1, 2, 3, 4};
-				listMonth = Arrays.asList(list);
+//			if(semester.contains("Spring")) {
+//				Integer [] list = {1, 2, 3, 4};
+//				listMonth = Arrays.asList(list);
+//			}
+//			else if(semester.contains("Summer")) {
+//				Integer [] list = {5, 6, 7, 8};
+//				listMonth = Arrays.asList(list);
+//			}
+//			else if(semester.contains("Fall")){
+//				Integer [] list = {9, 10, 11, 12};
+//				listMonth = Arrays.asList(list);
+//			}
+			
+			Optional<Semester> semesterOp = semesterRepository.findByName(semesterName);
+			if (semesterOp.isPresent()) {
+				Semester semester = semesterOp.get();
+				for (LocalDate date = semester.getStartDate(); date.isBefore(semester.getEndDate()); date.plusMonths(1)) {
+					listMonth.add(date.getMonthValue());
+				}
 			}
-			else if(semester.contains("Summer")) {
-				Integer [] list = {5, 6, 7, 8};
-				listMonth = Arrays.asList(list);
-			}
-			else if(semester.contains("Fall")){
-				Integer [] list = {9, 10, 11, 12};
-				listMonth = Arrays.asList(list);
-			}
+			
 			responseMessage.setData(listMonth);
 			responseMessage.setMessage("Lấy danh sách tháng thành công");
 		} catch (Exception e) {
