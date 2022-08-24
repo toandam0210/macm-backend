802dfb459d57a881c0bea1d02ec28ae828be47d5
Pham Minh Duc <menly10x@gmail.com>
2022-08-24 13:06:57 +0700

    setting role trong event

--- src/main/java/com/fpt/macm/service/RoleEventService.java
+++ src/main/java/com/fpt/macm/service/RoleEventService.java
@@ -5,6 +5,10 @@ import com.fpt.macm.model.response.ResponseMessage;
 public interface RoleEventService {
 
 	ResponseMessage getAllRoleEvent();
+	
+	ResponseMessage addNewRoleEvent(String newName);
 
 	ResponseMessage updateRoleEventName(int roleEventId, String newName);
+	
+	ResponseMessage updateStatusRoleEvent(int roleEventId);
 }
--- src/main/java/com/fpt/macm/service/RoleEventServiceImpl.java
+++ src/main/java/com/fpt/macm/service/RoleEventServiceImpl.java
@@ -1,6 +1,8 @@
 package com.fpt.macm.service;
 
+import java.util.Arrays;
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -31,12 +33,73 @@ public class RoleEventServiceImpl implements RoleEventService {
 		}
 		return responseMessage;
 	}
+	
+	@Override
+	public ResponseMessage addNewRoleEvent(String newName) {
+		ResponseMessage responseMessage = new ResponseMessage();
+		try {
+			List<RoleEvent> rolesEvent = roleEventRepository.findAll();
+			for (RoleEvent roleEvent : rolesEvent) {
+				if (roleEvent.getName().toLowerCase().equals(newName.toLowerCase().trim())) {
+					responseMessage.setMessage("Đã có vai trò này");
+					return responseMessage;
+				}
+			}
+
+			RoleEvent roleEvent = new RoleEvent();
+			roleEvent.setName(newName);
+			roleEvent.setActive(true);
+			roleEventRepository.save(roleEvent);
+			
+			responseMessage.setData(Arrays.asList(roleEvent));
+			responseMessage.setMessage("Thêm vai trò mặc định cho sự kiện thành công");
+		} catch (Exception e) {
+			responseMessage.setMessage(e.getMessage());
+		}
+		return responseMessage;
+	}
 
 	@Override
 	public ResponseMessage updateRoleEventName(int roleEventId, String newName) {
 		ResponseMessage responseMessage = new ResponseMessage();
 		try {
-			List<RoleEvent> rolesEvent = roleEventRepository.findByIsActiveOrderByIdAsc(true);
+			List<RoleEvent> rolesEvent = roleEventRepository.findAll();
+			for (RoleEvent roleEvent : rolesEvent) {
+				if (roleEvent.getName().toLowerCase().equals(newName.toLowerCase().trim())) {
+					responseMessage.setMessage("Đã có vai trò này");
+					return responseMessage;
+				}
+			}
+
+			Optional<RoleEvent> roleEventOp = roleEventRepository.findById(roleEventId);
+			if (roleEventOp.isPresent()) {
+				RoleEvent roleEvent = roleEventOp.get();
+				roleEvent.setName(newName);
+				roleEventRepository.save(roleEvent);
+			} else {
+				responseMessage.setMessage("Không có vai trò này");
+			}
+		} catch (Exception e) {
+			responseMessage.setMessage(e.getMessage());
+		}
+		return responseMessage;
+	}
+
+	@Override
+	public ResponseMessage updateStatusRoleEvent(int roleEventId) {
+		ResponseMessage responseMessage = new ResponseMessage();
+		try {
+			Optional<RoleEvent> roleEventOp = roleEventRepository.findById(roleEventId);
+			if (roleEventOp.isPresent()) {
+				RoleEvent roleEvent = roleEventOp.get();
+				roleEvent.setActive(!roleEvent.isActive());
+				roleEventRepository.save(roleEvent);
+				
+				responseMessage.setData(Arrays.asList(roleEvent));
+				responseMessage.setMessage("Cập nhật trạng thái của vai trò mặc định trong sự kiện thành công");
+			} else {
+				responseMessage.setMessage("Không có vai trò này");
+			}
 		} catch (Exception e) {
 			responseMessage.setMessage(e.getMessage());
 		}
