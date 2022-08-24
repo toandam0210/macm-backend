dc2d5c844ec3db1d3d365571a9077a14cf6d669d
Pham Minh Duc <menly10x@gmail.com>
2022-08-24 16:00:33 +0700

    fix delete organizing committee

--- src/main/java/com/fpt/macm/controller/TournamentController.java
+++ src/main/java/com/fpt/macm/controller/TournamentController.java
@@ -6,7 +6,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -257,7 +256,7 @@ public class TournamentController {
 				HttpStatus.OK);
 	}
 
-	@DeleteMapping("/headclub/deletetournamentorganizingcommittee/{tournamentOrganizingCommitteeId}")
+	@PutMapping("/headclub/deletetournamentorganizingcommittee/{tournamentOrganizingCommitteeId}")
 	@PreAuthorize("hasAnyRole('ROLE_HeadClub','ROLE_HeadTechnique','ROLE_ViceHeadTechnique')")
 	ResponseEntity<ResponseMessage> deleteTournamentOrganizingCommittee(
 			@PathVariable(name = "tournamentOrganizingCommitteeId") int tournamentOrganizingCommitteeId) {
