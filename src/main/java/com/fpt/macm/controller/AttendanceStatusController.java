6051cede743b8904cfa09302e15ef973e2c5bcf9
toandam0210 <84827568+toandam0210@users.noreply.github.com>
2022-08-26 13:03:57 +0700

    test ws

--- src/main/java/com/fpt/macm/controller/AttendanceStatusController.java
+++ src/main/java/com/fpt/macm/controller/AttendanceStatusController.java
@@ -28,12 +28,11 @@ public class AttendanceStatusController {
 	@Autowired
     private SimpMessagingTemplate simpMessagingTemplate;
 	
-	@MessageMapping("/takeattendance/{studentId}/{trainingScheduleId}")
+	@PutMapping("/takeattendance/{studentId}/{trainingScheduleId}")
 	@PreAuthorize("hasAnyRole('ROLE_HeadClub','ROLE_ViceHeadClub','ROLE_HeadCulture','ROLE_ViceHeadCulture','ROLE_HeadCommunication','ROLE_ViceHeadCommunication','ROLE_HeadTechnique','ROLE_ViceHeadTechnique','ROLE_Treasurer')")
 	ResponseEntity<ResponseMessage> takeAttendanceByStudentId(@PathVariable(name = "studentId") String studentId,
 			@PathVariable(name = "trainingScheduleId") int trainingScheduleId, @RequestParam int status) {
 		ResponseMessage response = attendanceStatusService.takeAttendanceByStudentId(studentId, status, trainingScheduleId);
-		simpMessagingTemplate.convertAndSendToUser(studentId, "/private", response);
 		return new ResponseEntity<ResponseMessage>(
 				response,
 				HttpStatus.OK);
