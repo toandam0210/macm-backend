ffeeb2898456db71967b525e97c7a1d14e263980
toandam0210 <84827568+toandam0210@users.noreply.github.com>
2022-08-26 14:29:05 +0700

    Fix/bugs (#646)
    * fix bugs
    
    * fix bug update user
    
    * test websocket
    
    * fix file pom
    
    * fix filter by multiple fields
    
    * fix authorization
    
    * export excel
    
    * fix export excel
    
    * fix filter by month
    
    * fix filter
    
    * fix filter
    
    * update status user side
    
    * fix update status
    
    * fix update user
    
    * fix update user
    
    * fix status
    
    * fix user report
    
    * fix report
    
    * fix report
    
    * update status user
    
    * fix ut
    
    * fix update status user
    
    * fix update status user
    
    * fix update status
    
    * fix update tournament
    
    * test websocket
    
    * test ws
    
    * test ws
    
    * test private ws
    
    * test ws
    
    * test ws
    
    * fix api
    
    * test ws
    
    * fix attendance
    
    * fix timeout ws

---
+++ src/main/java/com/fpt/macm/controller/PingPongController.java
@@ -0,0 +1,28 @@
+package com.fpt.macm.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.Payload;
+import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.messaging.simp.SimpMessagingTemplate;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.fpt.macm.model.response.Message;
+
+@RestController
+@EnableScheduling
+public class PingPongController {
+	
+	@Autowired
+    SimpMessagingTemplate template;
+	
+	@MessageMapping("/message")
+    @SendTo("/chatroom/public")
+	@Scheduled(fixedDelay = 10000L)
+    public Message receiveMessage(@Payload Message message){
+		message.setMessage("Pong");
+        return message;
+    }
+}
