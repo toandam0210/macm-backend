ee57d9085a373373830d7eb1af19ab7305fd1182
toandam0210 <84827568+toandam0210@users.noreply.github.com>
2022-08-26 08:33:58 +0700

    test websocket

--- src/main/java/com/fpt/macm/controller/AttendanceStatusController.java
+++ src/main/java/com/fpt/macm/controller/AttendanceStatusController.java
@@ -3,7 +3,9 @@ package com.fpt.macm.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.messaging.simp.SimpMessageSendingOperations;
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.Payload;
+import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -12,6 +14,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.fpt.macm.model.response.Message;
 import com.fpt.macm.model.response.ResponseMessage;
 import com.fpt.macm.service.AttendanceStatusService;
 
@@ -22,14 +25,13 @@ public class AttendanceStatusController {
 	AttendanceStatusService attendanceStatusService;
 	
 	@Autowired
-    private SimpMessageSendingOperations messagingTemplate;
-
+    private SimpMessagingTemplate simpMessagingTemplate;
+	
 	@PutMapping("/takeattendance/{studentId}/{trainingScheduleId}")
 	@PreAuthorize("hasAnyRole('ROLE_HeadClub','ROLE_ViceHeadClub','ROLE_HeadCulture','ROLE_ViceHeadCulture','ROLE_HeadCommunication','ROLE_ViceHeadCommunication','ROLE_HeadTechnique','ROLE_ViceHeadTechnique','ROLE_Treasurer')")
 	ResponseEntity<ResponseMessage> takeAttendanceByStudentId(@PathVariable(name = "studentId") String studentId,
 			@PathVariable(name = "trainingScheduleId") int trainingScheduleId, @RequestParam int status) {
 		ResponseMessage response = attendanceStatusService.takeAttendanceByStudentId(studentId, status, trainingScheduleId);
-		messagingTemplate.convertAndSend("/message", response.getMessage());
 		return new ResponseEntity<ResponseMessage>(
 				response,
 				HttpStatus.OK);
@@ -40,7 +42,6 @@ public class AttendanceStatusController {
 	ResponseEntity<ResponseMessage> checkAttendanceByStudentId(
 			@PathVariable(name = "trainingScheduleId") int trainingScheduleId) {
 		ResponseMessage response = attendanceStatusService.checkAttendanceStatusByTrainingSchedule(trainingScheduleId);
-		messagingTemplate.convertAndSend("/message", response.getData());
 		return new ResponseEntity<ResponseMessage>(
 				response, HttpStatus.OK);
 	}
@@ -76,4 +77,11 @@ public class AttendanceStatusController {
 		return new ResponseEntity<ResponseMessage>(
 				attendanceStatusService.getAttendanceTrainingStatistic(semesterName, roleId), HttpStatus.OK);
 	}
+	
+	@MessageMapping("/private-message")
+    public Message recMessage(@Payload Message message){
+        simpMessagingTemplate.convertAndSendToUser(message.getReceiverName(),"/private",message);
+        System.out.println(message.toString());
+        return message;
+    }
 }
---
+++ src/main/java/com/fpt/macm/model/response/Message.java
@@ -0,0 +1,41 @@
+package com.fpt.macm.model.response;
+
+public class Message {
+    private String senderName;
+    private String receiverName;
+    private String message;
+    private String date;
+    private Status status;
+	public String getSenderName() {
+		return senderName;
+	}
+	public void setSenderName(String senderName) {
+		this.senderName = senderName;
+	}
+	public String getReceiverName() {
+		return receiverName;
+	}
+	public void setReceiverName(String receiverName) {
+		this.receiverName = receiverName;
+	}
+	public String getMessage() {
+		return message;
+	}
+	public void setMessage(String message) {
+		this.message = message;
+	}
+	public String getDate() {
+		return date;
+	}
+	public void setDate(String date) {
+		this.date = date;
+	}
+	public Status getStatus() {
+		return status;
+	}
+	public void setStatus(Status status) {
+		this.status = status;
+	}
+    
+    
+}
